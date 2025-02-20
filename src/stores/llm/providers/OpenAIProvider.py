fa8e28314888c86711956e1eb1ad039f989575a4
Amro Rabea <amroalsafy@gmail.com>
2025-02-20 16:04:08 +0200

    create nlp routers

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -26,7 +26,7 @@ class OpenAIProvider(LLMInterface):
 
         self.client = OpenAI(
             api_key = self.api_key,
-            api_url = self.api_url
+            # api_url = self.api_url
         )
 
         self.logger = logging.getLogger(__name__)
